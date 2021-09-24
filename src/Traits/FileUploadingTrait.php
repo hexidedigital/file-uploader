6af275c4ad71933688a23d90c095e8fb943e1861
moik.o <moik.o@hexide-digital.com>
2021-09-24 08:37:00 +0300

    trait for handle your request with image or image

---
+++ src/Traits/FileUploadingTrait.php
@@ -0,0 +1,93 @@
+<?php
+
+namespace HexideDigital\FileUploader\Traits;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Http\Request;
+use Illuminate\Http\UploadedFile;
+use HexideDigital\FileUploader\Facades\FileUploader;
+
+trait FileUploadingTrait
+{
+
+    /**
+     * @param array $images
+     * @param string|null $uniq_id to place in the same folder
+     * @param string|null $type default is `photos`
+     * @param string|null $module
+     * @return array[]
+     */
+    public function saveImages(array $images, string $type = null, string $uniq_id = null, string $module = null): array
+    {
+        $result = [];
+
+        foreach ($images as $image) {
+            $result[] = $this->saveImage($image, $uniq_id, null, $type, $module);
+        }
+
+        return ['attached' => $result];
+    }
+
+    /**
+     * @param UploadedFile|mixed|null $image
+     * @param string|null $uniq_id to place in the same folder
+     * @param string|null $old_path to delete old photo
+     * @param string|null $type default is `images`
+     * @param string|null $module
+     * @return string|null
+     */
+    public function saveImage($image,
+                              string $uniq_id = null, string $old_path = null,
+                              string $type = null, string $module = null): ?string
+    {
+        if (!empty($old_path)) {
+            $this->deleteImage($old_path);
+        }
+
+        if (empty($module)) {
+            $module = $this->module ?? '';
+        }
+
+        if (empty($type)) {
+            $type = 'images';
+        }
+
+        return FileUploader::put($image, $type, $module, $uniq_id) ?? null;
+    }
+
+    public function deleteImage(?string $path): bool
+    {
+        return FileUploader::delete($path);
+    }
+
+    /**
+     * @param Request $request
+     * @param Model|null $model
+     * @param array $options =
+     * [
+     *  'field_key' => 'image',
+     *  'folder' => 'images',
+     * ]
+     * @return bool
+     */
+    public function handleOneImage(Request $request, ?Model $model = null, array $options = [])
+    {
+        $path = false;
+
+        if(empty($options['field_key'])) $options['field_key'] = 'image';
+        if(empty($options['folder'])) $options['folder'] = 'images';
+
+        $old_path = $model->{$options['field_key']} ?? null;
+
+        if ($request->hasFile('image') || $request->input('isRemoveImage', false)) {
+            $path = $this->saveImage(
+                $request->file('image'),
+                $request->input('slug'),
+                $old_path,
+                $options['folder']
+            );
+        }
+
+        return $path;
+    }
+}
